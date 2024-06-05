6ed281a21e38e05981a39fbdbf7f79b467d667fb
Alan Hu <alan.notiverson@gmail.com>
2024-06-05 17:04:34 -0400

    Write function to display all books to the screen

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, num_pages, read) {
     this.title = title;
     this.author = author;
@@ -16,3 +18,51 @@ function Book(title, author, num_pages, read) {
         return output;
     }
 }
+
+const addBookToLibrary = (book) => {
+    myLibrary.push(book);
+};
+
+const displayAllBooks = (book) => {
+
+    function hasRead(has_read) {
+        let read_book;
+        if (has_read) {
+            read_book = 'yes';
+        } else {
+            read_book = 'no';
+        }
+        return read_book;
+    }
+
+    const page = document.querySelector('body');
+    const booksContainer = document.createElement('div');
+    booksContainer.classList.add('book-container');
+    for (const book of myLibrary) {
+        const bookCard = document.createElement('div');
+        const bookTitle = document.createElement('h3');
+        bookTitle.textContent = `Title: ${book['title']}`;
+        const bookAuthor = document.createElement('h3');
+        bookAuthor.textContent = `Author: ${book['author']}`;
+        const bookPages = document.createElement('h3');
+        bookPages.textContent = `Pages: ${book['num_pages']}`;
+        const readBook = document.createElement('h3');
+        readBook.textContent = `Read: ${hasRead(book['read'])}`;
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(bookPages);
+        bookCard.append(readBook);
+        bookCard.style.border = '2px solid black';
+        bookCard.style.borderRadius = '16px';
+        booksContainer.appendChild(bookCard);
+    }
+    page.appendChild(booksContainer);
+};
+
+const diaryOfAWimpyKid = new Book('Diary of a Wimpy Kid', 'Jeff Kinney', 224, true);
+const dogDays = new Book('Dog Days', 'Jeff Kinney', 217, true);
+
+addBookToLibrary(diaryOfAWimpyKid);
+addBookToLibrary(dogDays);
+
+displayAllBooks();
