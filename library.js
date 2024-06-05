8e4069af81176264b59e2d332802b6ac68da11ce
Alan Hu <alan.notiverson@gmail.com>
2024-06-05 14:59:38 -0400

    Add files

---
+++ library.js
@@ -0,0 +1,10 @@
+function Book(title, author, num_pages, read) {
+    this.title = title;
+    this.author = author;
+    this.num_pages = num_pages;
+    this.read = read;
+
+    this.info = function() {
+
+    }
+}
